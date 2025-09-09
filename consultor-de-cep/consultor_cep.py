7e6c3c6c42d1c8b00d3c335041ac2bd50b8b949d
grabaloss <dgrabalos.lucas@gmail.com>
2025-09-09 02:04:09 -0300

    feat: Adiciona a funcionalidade de salvar endereço em arquivo

--- consultor-de-cep/consultor_cep.py
+++ consultor-de-cep/consultor_cep.py
@@ -1,16 +1,40 @@
 import requests
 
-cep = input('Digite o CEP (apenas números): ')
-url = f'https://viacep.com.br/ws/{cep}/json/'
+def consultar_cep(cep):
+    
+    url = f'https://viacep.com.br/ws/{cep}/json/'
 
-resposta = requests.get(url)
+    try:
+        resposta = requests.get(url)
+        resposta.raise_for_status()
+        dados = resposta.json()
+       
+        if "erro" in dados:
+            return None 
+        return dados
+    
+    except requests.exceptions.RequestException as e:
+        print(f'Ocorreu um erro na requisição {e}')
+        return None
+    
+print('--- Bem vindo ao Consultor de CEP ---')
+cep_usuario = input('Digite o CEP que deseja consultar (apenas número): ')
 
-if resposta.status_code == 200: 
-    dados = resposta.json()
-    print('CEP:', dados['cep'])
-    print('Logradouro:', dados['logradouro'])
-    print('Bairro:', dados['bairro'])
-    print('Cidade:', dados['localidade'])
-    print('Estado:', dados['uf'])
-else: 
-    print('CEP não encontrado.')
+endereco_encontrado = consultar_cep(cep_usuario)
+
+if endereco_encontrado:
+    print('\n--- Endereço Encontrado ---')
+    print(f"Logradouro: {endereco_encontrado['logradouro']}")
+    print(f"Bairro: {endereco_encontrado['bairro']}")
+    print(f"Cidade: {endereco_encontrado['localidade']}")
+    print(f"Estado: {endereco_encontrado['uf']}")
+    print(f"CEP: {endereco_encontrado['cep']}")
+
+    cep = endereco_encontrado['cep']
+    estado = endereco_encontrado['uf']
+    cidade = endereco_encontrado['localidade']
+    bairro = endereco_encontrado['bairro']
+    logradouro = endereco_encontrado['logradouro']
+
+else:
+    print(f"Não foi possível encontrar o endereço para o CEP {cep_usuario}. Verifique se ele está correto.")
